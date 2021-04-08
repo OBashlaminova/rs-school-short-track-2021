4a7d9658b4557adbcdcc25047a74c809337ba777
OBashlaminova <olga.bashlaminova.ob@gmail.com>
2021-04-08 14:13:28 +0100

    feat: add 07-file-names realization

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -1,20 +1,19 @@
-/**
- * There's a list of file, since two files cannot have equal names,
- * the one which comes later will have a suffix (k),
- * where k is the smallest integer such that the found name is not used yet.
- *
- * Return an array of names that will be given to the files.
- *
- * @param {Array} names
- * @return {Array}
- *
- * @example
- * For input ["file", "file", "image", "file(1)", "file"],
- * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
- *
- */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(fileArray) {
+  const resultFileArray = [];
+  for (let i = 0; i < fileArray.length; i++) {
+    const fileName = fileArray[i];
+    resultFileArray[i] = fileName;
+    let k = 0;
+    for (let j = 0; j < i; j++) {
+      if (fileName === resultFileArray[j] || fileName === fileArray[j]) {
+        k++;
+      }
+      if (j === i - 1 && k !== 0) {
+        resultFileArray[i] += `(${k})`;
+      }
+    }
+  }
+  return resultFileArray;
 }
 
 module.exports = renameFiles;
