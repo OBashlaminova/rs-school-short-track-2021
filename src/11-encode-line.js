9c5eee54067da200f48094b98c0a68fad40fec70
OBashlaminova <olga.bashlaminova.ob@gmail.com>
2021-04-08 18:29:52 +0100

    feat: add 11-encode-line realization

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -1,15 +1,21 @@
-/**
- * Given a string, return its encoding version.
- *
- * @param {String} str
- * @return {String}
- *
- * @example
- * For aabbbc should return 2a3bc
- *
- */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const array = str.split('');
+  let result = '';
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === array[i + 1]) {
+      count++;
+    } else {
+      count++;
+      if (i === 0 || count === 1) {
+        result += `${array[i]}`;
+      } else {
+        result += `${count}${array[i - 1]}`;
+      }
+      count = 0;
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
