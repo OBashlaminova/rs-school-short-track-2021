dbc4da9421ebc8488c73302fdb80d64bb2b46518
OBashlaminova <olga.bashlaminova.ob@gmail.com>
2021-04-09 14:47:44 +0100

    feat: add 04-mine-sweeper realization

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -1,28 +1,24 @@
-/**
- * In the popular Minesweeper game you have a board with some mines and those cells
- * that don't contain a mine have a number in it that indicates the total number of mines
- * in the neighboring cells. Starting off with some arrangement of mines
- * we want to create a Minesweeper game setup.
- *
- * @param {Array<Array>} matrix
- * @return {Array<Array>}
- *
- * @example
- * matrix = [
- *  [true, false, false],
- *  [false, true, false],
- *  [false, false, false]
- * ]
- *
- * The result should be following:
- * [
- *  [1, 2, 1],
- *  [2, 1, 1],
- *  [1, 1, 1]
- * ]
- */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    result[i] = [];
+    const prevRow = matrix[i - 1];
+    const currRow = matrix[i];
+    const nextRow = matrix[i + 1];
+    for (let j = 0; j < matrix[i].length; j++) {
+      let counter = 0;
+      if (nextRow && nextRow[j] === true) counter++; // up
+      if (prevRow && prevRow[j] === true) counter++; // down
+      if (currRow[j + 1] === true) counter++; // right
+      if (nextRow && nextRow[j + 1] === true) counter++; // up + right
+      if (prevRow && prevRow[j + 1] === true) counter++; // down + right
+      if (currRow[j - 1] === true) counter++; // left
+      if (nextRow && nextRow[j - 1] === true) counter++; // up + left
+      if (prevRow && prevRow[j - 1] === true) counter++; // down + left
+      result[i][j] = counter;
+    }
+  }
+  return result;
 }
 
 module.exports = minesweeper;
