038140f5cec8f365690e1b813601039e84c0e55a
OBashlaminova <olga.bashlaminova.ob@gmail.com>
2021-04-09 10:29:35 +0100

    feat: add 13-stack realization

--- src/13-stack.js
+++ src/13-stack.js
@@ -1,26 +1,21 @@
-/**
- * Implement the Stack with a given interface via array.
- *
- * @example
- * const stack = new Stack();
- *
- * stack.push(1); // adds the element to the stack
- * stack.peek(); // returns the peek, but doesn't delete it, returns 1
- * stack.pop(); // returns the top element from stack and deletes it, returns 1
- * stack.pop(); // undefined
- *
- */
 class Stack {
-  push(/* element */) {
-    throw new Error('Not implemented');
+  constructor() {
+    this.data = [];
+    this.top = 0;
+  }
+
+  push(element) {
+    this.data[this.top] = element;
+    this.top++;
   }
 
   pop() {
-    throw new Error('Not implemented');
+    this.top--;
+    return this.data.pop();
   }
 
   peek() {
-    throw new Error('Not implemented');
+    return this.data[this.top - 1];
   }
 }
 
