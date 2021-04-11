3a40d64f62dcd74146486f28fda83d5daf0c838f
OBashlaminova <olga.bashlaminova.ob@gmail.com>
2021-04-11 12:29:46 +0100

    feat: add 12-dns-stats realization

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -1,27 +1,24 @@
-/**
- * Given an array of domains, return the object with the appearances of the DNS.
- *
- * @param {Array} domains
- * @return {Object}
- *
- * @example
- * domains = [
- *  'code.yandex.ru',
- *  'music.yandex.ru',
- *  'yandex.ru'
- * ]
- *
- * The result should be the following:
- * {
- *   '.ru': 3,
- *   '.ru.yandex': 3,
- *   '.ru.yandex.code': 1,
- *   '.ru.yandex.music': 1,
- * }
- *
- */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domainsArray) {
+  return domainsArray.reduce((result, domain) => {
+    const parts = domain.split('.').reverse();
+    const nextResult = result;
+
+    const joinedParts = parts.reduce((reversedDomains, part, index) => {
+      const previousPart = reversedDomains[index - 1];
+      reversedDomains.push(`${previousPart || ''}.${part}`);
+      return reversedDomains;
+    }, []);
+
+    joinedParts.forEach((part) => {
+      if (result[part]) {
+        nextResult[part]++;
+      } else {
+        nextResult[part] = 1;
+      }
+    });
+
+    return nextResult;
+  }, {});
 }
 
 module.exports = getDNSStats;
